370d9373239a019500f8f84500e50273b9daad12
Stas <stas.logvin.99@gmail.com>
2020-05-26 19:29:41 +0300

    Removed @Transactional from PhoneDao

--- core/src/main/java/com/es/core/model/phone/PhoneDao.java
+++ core/src/main/java/com/es/core/model/phone/PhoneDao.java
@@ -1,11 +1,8 @@
 package com.es.core.model.phone;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
-@Transactional
 public interface PhoneDao {
     Optional<Phone> get(Long key);
     void save(Phone phone);
